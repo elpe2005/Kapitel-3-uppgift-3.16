f275bb37530c9f08efb9a2ada7ba7088fdd42a11
elpe2005 <epedersen2005@gmail.com>
2022-10-05 12:15:46 +0200

    Klar med uppgift 3.16

--- Kapitel 3 uppgift 3.16/Program.cs
+++ Kapitel 3 uppgift 3.16/Program.cs
@@ -5,6 +5,21 @@ namespace Uppgift313
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Du får välja en låt, skriv in låtens tid här.");
+            int minuter =int.Parse(Console.ReadLine());
+            int sekunder = int.Parse(Console.ReadLine());
+
+            int sekunder2 = (minuter * 60 + sekunder);
+            if (minuter >=165  && minuter <=260 )
+            {
+                 Console.WriteLine("Din sång kan få spelas.");
+            }
+            else
+            {
+                Console.WriteLine("Din sång kan inte spelas");
+            }
+
+
 
         }
     }
